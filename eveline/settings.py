e5811313c73c098a95b1ce00628d387ef68394ab
pilecki <mlaskowski@onet.pl>
2020-10-13 02:07:11 +0000

    Setup a project and added home app

--- eveline/settings.py
+++ eveline/settings.py
@@ -122,7 +122,8 @@ USE_TZ = True
 
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'eveline/static'),)
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'eveline/media')
+
